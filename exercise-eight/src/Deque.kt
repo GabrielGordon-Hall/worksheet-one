58cc255c0f95a96233a9d824f38994590a560a5a
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-01-13 00:58:46 +0000

    'Last question'

--- exercise-eight/src/Deque.kt
+++ exercise-eight/src/Deque.kt
@@ -2,42 +2,42 @@ class Deque<T> {
     private var head: Element<T>? = null
 
     fun push(value: T) {
-        if (head == null) {
-            head = Element(value, null, null)
-        } else {
+        if (head != null) {
             var pointer = head
             while (pointer?.next != null) {
                 pointer = pointer.next
             }
-            Element(value, pointer, null)
+            val tail = Element(value, pointer, null)
+            pointer?.next = tail
+        } else {
+            head = Element(value, null, null)
         }
     }
 
     fun pop(): T? {
         var pointer = head
+        var pointerPrev: Element<T>? = null
         while (pointer?.next != null) {
+            pointerPrev = pointer
             pointer = pointer.next
         }
         val res = pointer?.value
-        pointer = Element(null, pointer?.prev, null)
+        pointerPrev?.next = null
         return res
     }
 
     fun unshift(value: T) {
-//        val newHead = Element(value, null, head)
-//        head = Element(head?.value, newHead, head?.next)
+        val oldHead = head
+        val newHead = Element(value, null, oldHead)
+        oldHead?.prev = newHead
+        head = newHead
     }
 
     fun shift(): T? {
-        return head?.value
+        val res = head?.value
+        head = head?.next
+        return res
     }
 
     private data class Element<T>(val value: T, var prev: Element<T>? = null, var next: Element<T>? = null)
-}
-
-fun main(args: Array<String>) {
-    var link = Deque<Int>()
-    link.push(5)
-    link.push(10)
-    println(link.shift())
 }
